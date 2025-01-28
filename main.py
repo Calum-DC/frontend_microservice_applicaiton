10f991a047f2879372b55deb14233aa5a2198135
Calum Courtney <Courtneycd@cardiff.ac.uk>
2025-01-28 21:03:20 +0000

    minor tweaks

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from flask  import Flask, render_template, redirect_url, jsonify
 
 app = Flask(__name__)
 
-#
-# https://sqs.eu-north-1.amazonaws.com/535002885105/SQS_Queue_P1
-# https://sqs.eu-north-1.amazonaws.com/535002885105/SQS_Queue_P2
-# https://sqs.eu-north-1.amazonaws.com/535002885105/SQS_Queue_P3
-
-
 
 
 
